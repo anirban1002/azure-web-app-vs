8afbd70ff2988934a5e8a7cd41041d5dc6874c20
anirban1002 <anirbanhalder.cse@gmail.com>
2023-05-29 19:28:46 +0530

    ef-core

---
+++ azure-web-app-vs/Pages/Persons/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using azure_web_app_vs.Data;
+
+namespace azure_web_app_vs.Pages.Persons
+{
+    public class IndexModel : PageModel
+    {
+        private readonly azure_web_app_vs.Data.AppDbContext _context;
+
+        public IndexModel(azure_web_app_vs.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Person> Person { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Persons != null)
+            {
+                Person = await _context.Persons.ToListAsync();
+            }
+        }
+    }
+}
