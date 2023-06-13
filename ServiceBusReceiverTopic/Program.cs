f643ba41d8b61784056dc782a18b95bdc225f2a2
anirban1002 <anirbanhalder.cse@gmail.com>
2023-06-13 18:24:51 +0530

    azure-function

---
+++ ServiceBusReceiverTopic/Program.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "Endpoint=sb://az-servicebus-learning-standard.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=jxPSaxwUW0hqtO5buOMKI4/tF4VaMCkOa+ASbOa1CRs=";
+const string topicName = "az-servicebus-learning-topic";
+const string subscriptionName = "az-servicebus-learning-topic-subscription1";
+
+ServiceBusClient client;
+ServiceBusProcessor processor = default!;
+
+async Task MessageHandler(ProcessMessageEventArgs processMessageEventArgs)
+{
+    string body = processMessageEventArgs.Message.Body.ToString();
+    Console.WriteLine($"{body} - Subscription: {subscriptionName}");
+    await processMessageEventArgs.CompleteMessageAsync(processMessageEventArgs.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs processErrorEventArgs)
+{
+    Console.WriteLine(processErrorEventArgs.Exception.ToString());
+    return Task.CompletedTask;
+}
+
+client = new ServiceBusClient(serviceBusConnectionString);
+processor = client.CreateProcessor(topicName, subscriptionName, new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+    Console.WriteLine("Press any key to end processing");
+    Console.ReadLine();
+
+    Console.WriteLine("\nStopping the receiver....");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving message");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
---
+++ ServiceBusSenderTopic/Program.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "Endpoint=sb://az-servicebus-learning-standard.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=jxPSaxwUW0hqtO5buOMKI4/tF4VaMCkOa+ASbOa1CRs=";
+const string topicName = "az-servicebus-learning-topic";
+const int maxNumberOfMessages = 3;
+
+ServiceBusClient client;
+ServiceBusSender sender;
+
+client = new ServiceBusClient(serviceBusConnectionString);
+sender = client.CreateSender(topicName);
+
+using (ServiceBusMessageBatch batch = await sender.CreateMessageBatchAsync())
+{
+    for (int i = 1; i <= maxNumberOfMessages; i++)
+    {
+        if (!batch.TryAddMessage(new ServiceBusMessage($"This is a message - {i}")))
+        {
+            Console.WriteLine($"message - {i} was not added to the batch");
+        }
+    }
+
+    try
+    {
+        await sender.SendMessagesAsync(batch);
+        Console.WriteLine("Messages Sent");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exception:{ex.Message}");
+    }
+    finally
+    {
+        await sender.DisposeAsync();
+        await client.DisposeAsync();
+    }
+}
